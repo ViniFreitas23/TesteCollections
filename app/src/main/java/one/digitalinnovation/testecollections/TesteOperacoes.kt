71c9360b756fb57ea8ce90db35d8bfbf0a477c31
ViniFreitas23 <vinicius.freitas.correia@hotmail.com>
2021-07-19 10:36:48 -0300

    Updated Operations test class

--- app/src/main/java/one/digitalinnovation/testecollections/TesteOperacoes.kt
+++ app/src/main/java/one/digitalinnovation/testecollections/TesteOperacoes.kt
@@ -1,7 +1,5 @@
 package one.digitalinnovation.testecollections
 
-import kotlin.math.max
-
 fun main() {
     val salarios = doubleArrayOf(1000.0, 2250.0, 4000.0)
 
@@ -16,4 +14,14 @@ fun main() {
     println("----------")
     salariosMaiorQue2500.forEach{ println(it)}
 
+    println("----------")
+    println(salarios.count { it in 2000.0..5000.0 })
+
+    println("----------")
+    println(salarios.find { it == 2250.0 })
+    println(salarios.find { it == 250.0 })
+
+    println("----------")
+    println(salarios.any { it == 1000.0 })
+    println(salarios.any { it == 500.0 })
 }
