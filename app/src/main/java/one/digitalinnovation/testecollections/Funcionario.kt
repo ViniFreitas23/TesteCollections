3cf7e7e10c74209b771df5718f4fc330c806e4c8
ViniFreitas23 <vinicius.freitas.correia@hotmail.com>
2021-07-19 14:02:48 -0300

    Created List and Set test. Created Funcionario data class

---
+++ app/src/main/java/one/digitalinnovation/testecollections/Funcionario.kt
@@ -0,0 +1,13 @@
+package one.digitalinnovation.testecollections
+
+data class Funcionario(
+    val nome: String,
+    val salario: Double,
+    val tipoContrato: String
+) {
+    override fun toString(): String =
+        """
+            Nome:    $nome
+            Salário: $salario
+        """.trimIndent()
+}
